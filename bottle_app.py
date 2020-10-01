0d4f1c53092b65859f2f5346329ccd27bea75c0c
jschnip1 <jschnip1@kent.edu>
2020-10-01 01:48:40 +0000

    changed stuff from class

--- bottle_app.py
+++ bottle_app.py
@@ -21,6 +21,14 @@ def get_show_list():
     cursor.close()
     return template("show_list", rows=result)
 
+@get('/set_status/<id:int>/<value:int>')
+def get_set_status(id,value):
+    connection = sqlite3.connect("todo.db")
+    cursor = connection.cursor()
+    cursor.execute("update todo set status= 0 where id =?", (id, value,))
+    connection.commit()
+    cursor.close()
+    redirect('/')
 
 @get('/new_item')
 def get_new_item():
@@ -46,6 +54,28 @@ def get_delete_item(id):
     connection.commit()
     cursor.close()
     redirect('/')
+
+@get('/update_item/<id:int>')
+def get_update_item(id):
+    connection = sqlite3.connect("todo.db")
+    cursor = connection.cursor()
+    cursor.execute("select * from todo where id=?", (id,))
+    result = cursor.fetchall()
+    cursor.close()
+    return template("update_item", row=result[0])
+
+
+@post('/update_item')
+def post_update_item():
+    id = int(request.form.get("id").strip())
+    updated_item = request.forms.get("updated_item").strip()
+    connection = sqlite3.connect("todo.db")
+    cursor = connection.cursor()
+    cursor.execute("update todo set task=? where id =?", (id, updated_item,))
+    connection.commit()
+    cursor.close()
+    redirect('/')
+
 
 if ON_PYTHONANYWHERE:
     application = default_app()
